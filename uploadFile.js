c2e357a66762501a36a413c1f5e6721ea8bbfe99
Abhishek-1310 <kumaryadavabhi1999@gmail.com>
2025-07-02 16:21:37 +0530

    changed bucket

--- uploadFile.js
+++ uploadFile.js
@@ -14,7 +14,7 @@ exports.handler = async (event) => {
         return {
             statusCode: 200,
             headers: corsHeaders,
-            body: JSON.stringify({ message: "CORS preflight handled done" }),
+            body: JSON.stringify({ message: "CORS preflight handled" }),
         };
     }
 
